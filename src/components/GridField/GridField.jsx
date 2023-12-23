4a7d4e3acf7d698e961460a89b077fa1c85bb93a
NikitaKornik <nikita0927@mail.ru>
2023-12-23 20:54:10 +0200

    added logic

--- src/components/GridField/GridField.jsx
+++ src/components/GridField/GridField.jsx
@@ -4,107 +4,153 @@ import btnS from "../UlLib/Btn/Btn.module.scss";
 import s from "./GridField.module.scss";
 
 const orange = {
-  origin: "tools",
+  origin: "sign",
   className: btnS.orange,
   // backgroundColor: "#f6a902",
 };
 const grey = {
+  origin: "tools",
   className: btnS.grey,
   // backgroundColor: "#b0b0b0",
   // color: "#000000",
 };
 const darkgrey = {
+  origin: "digital",
   className: btnS.darkgrey,
   // backgroundColor: "#3c3c3c",
 };
 
 export function GridField(props) {
+  const allBtns = [
+    {
+      name: "C",
+      color: grey,
+    },
+    {
+      name: "+/-",
+      color: grey,
+    },
+    {
+      name: "%",
+      color: grey,
+    },
+    {
+      name: "/",
+      color: orange,
+    },
+    {
+      name: "7",
+      color: darkgrey,
+    },
+    {
+      name: "8",
+      color: darkgrey,
+    },
+    {
+      name: "9",
+      color: darkgrey,
+    },
+    {
+      name: "*",
+      color: orange,
+    },
+    {
+      name: "4",
+      color: darkgrey,
+    },
+    {
+      name: "5",
+      color: darkgrey,
+    },
+    {
+      name: "6",
+      color: darkgrey,
+    },
+    {
+      name: "-",
+      color: orange,
+    },
+    {
+      name: "1",
+      color: darkgrey,
+    },
+    {
+      name: "2",
+      color: darkgrey,
+    },
+    {
+      name: "3",
+      color: darkgrey,
+    },
+    {
+      name: "+",
+      color: orange,
+    },
+    {
+      name: "0",
+      color: darkgrey,
+    },
+    {
+      name: ".",
+      color: darkgrey,
+    },
+    {
+      name: "=",
+      color: orange,
+    },
+  ];
+
   const [isActive, setIsActive] = useState(1);
+
+  const [firstDigital, setFirstDigital] = useState("");
+  const [secondDigital, setSecondDigital] = useState("");
+  const [sign, setSign] = useState("");
+  const [finish, setFinish] = useState(false);
+  const [display, setDisplay] = useState("0");
+
+  if (finish == true) {
+    switch (sign) {
+      case "+":
+        setFirstDigital(+firstDigital + +secondDigital);
+        break;
+      case "-":
+        setFirstDigital(+firstDigital - +secondDigital);
+        break;
+      case "*":
+        setFirstDigital(+firstDigital * +secondDigital);
+        break;
+      case "/":
+        setFirstDigital(+firstDigital / +secondDigital);
+        break;
+    }
+    setFinish(false);
+    setSecondDigital("");
+    if (sign == "=") {
+      setSign("");
+    }
+  }
+
+  const renderBtns = allBtns.map((e, index) => (
+    <div className={`${s.cell} ${s.cell_}`}>
+      <Btn
+        color={e.color}
+        name={e.name}
+        firstDigital={firstDigital}
+        setFirstDigital={setFirstDigital}
+        secondDigital={secondDigital}
+        setSecondDigital={setSecondDigital}
+        sign={sign}
+        setSign={setSign}
+        finish={finish}
+        setFinish={setFinish}
+        setDisplay={setDisplay}
+      />
+    </div>
+  ));
   return (
     <div className={s.grid}>
-      <div className={s.answer}>answer</div>
-      <div className={`${s.cell} ${s.cell_ac}`}>
-        <Btn color={grey} name="C" />
-      </div>
-      <div className={`${s.cell} ${s.cell_PlMn}`}>
-        <Btn color={grey} name="+/-" />
-      </div>
-      <div className={`${s.cell} ${s.cell_p}`}>
-        <Btn color={grey} name="%" />
-      </div>
-      <div className={`${s.cell} ${s.cell_div}`}>
-        <Btn
-          color={orange}
-          name="/"
-          globalActive={isActive}
-          setGlobalActive={setIsActive}
-        />
-      </div>
-      <div className={`${s.cell} ${s.cell_multip}`}>
-        <Btn color={darkgrey} name="7" />
-      </div>
-      <div className={`${s.cell} ${s.cell_minus}`}>
-        <Btn color={darkgrey} name="8" />
-      </div>
-      <div className={`${s.cell} ${s.cell_plus}`}>
-        <Btn color={darkgrey} name="9" />
-      </div>
-      <div className={`${s.cell} ${s.cell_equals}`}>
-        <Btn
-          color={orange}
-          name="*"
-          globalActive={isActive}
-          setGlobalActive={setIsActive}
-        />
-      </div>
-      <div className={`${s.cell} ${s.cell_1}`}>
-        <Btn color={darkgrey} name="4" />
-      </div>
-      <div className={`${s.cell} ${s.cell_2}`}>
-        <Btn color={darkgrey} name="5" />
-      </div>
-      <div className={`${s.cell} ${s.cell_3}`}>
-        <Btn color={darkgrey} name="6" />
-      </div>
-      <div className={`${s.cell} ${s.cell_4}`}>
-        <Btn
-          color={orange}
-          name="-"
-          globalActive={isActive}
-          setGlobalActive={setIsActive}
-        />
-      </div>
-      <div className={`${s.cell} ${s.cell_5}`}>
-        <Btn color={darkgrey} name="1" />
-      </div>
-      <div className={`${s.cell} ${s.cell_6}`}>
-        <Btn color={darkgrey} name="2" />
-      </div>
-      <div className={`${s.cell} ${s.cell_7}`}>
-        <Btn color={darkgrey} name="3" />
-      </div>
-      <div className={`${s.cell} ${s.cell_8}`}>
-        <Btn
-          color={orange}
-          name="+"
-          globalActive={isActive}
-          setGlobalActive={setIsActive}
-        />
-      </div>
-      <div className={`${s.cell} ${s.cell_9}`}>
-        <Btn color={darkgrey} name="0" />
-      </div>
-      <div className={`${s.cell} ${s.cell_0}`}>
-        <Btn color={darkgrey} name="," />
-      </div>
-      <div className={`${s.cell} ${s.cell_dot}`}>
-        <Btn
-          color={orange}
-          name="="
-          globalActive={isActive}
-          setGlobalActive={setIsActive}
-        />
-      </div>
+      <div className={s.answer}>{display}</div>
+      {renderBtns}
     </div>
   );
 }
--- src/components/UlLib/Btn/Btn.jsx
+++ src/components/UlLib/Btn/Btn.jsx
@@ -1,18 +1,90 @@
 import { useState } from "react";
 import s from "./Btn.module.scss";
 
-export function Btn({ name, color, globalActive, setGlobalActive }) {
+export function Btn({
+  name, // имя (1, 2 ... *)
+  color, // объект с данными кнопки (цвет, значение)
+  globalActive, // активность кнопки (/ * + - =)
+  //вся информация о первом числе, втором числе, операторе, заключения
+  firstDigital,
+  setFirstDigital,
+  secondDigital,
+  setSecondDigital,
+  sign,
+  setSign,
+  finish,
+  setFinish,
+  setDisplay,
+}) {
   // const [active, setActive] = useState(false);
   const [isClicked, setIsClicked] = useState(false);
+  // const [test, setTest] = useState("");
   function onclick() {
-    if (color.origin === "tools") {
-      setGlobalActive(name);
+    if (color.origin == "tools") {
+      if (name == "C") {
+        setFirstDigital("");
+        setSecondDigital("");
+        setSign("");
+        setFinish(false);
+        setDisplay("0");
+        console.log("clear");
+      }
+      if (name == "+/-") {
+        if (sign != "") {
+          setSecondDigital(secondDigital * -1);
+        } else {
+          setFirstDigital(firstDigital * -1);
+        }
+      }
+      if (name == "%") {
+        if (sign != "") {
+          setSecondDigital(secondDigital / 100);
+        } else {
+          setFirstDigital(firstDigital / 100);
+        }
+      }
     }
-    setIsClicked(true);
-    setTimeout(() => {
-      setIsClicked(false);
-    }, 100);
+    if (color.origin == "digital") {
+      if (sign == "") {
+        setFirstDigital(firstDigital + name);
+      } else if (sign != "") {
+        setSecondDigital(secondDigital + name);
+      }
+    }
+    if (color.origin == "sign") {
+      if (sign == "" && firstDigital != "" && name != "=") {
+        setSign(name);
+      }
+      if (sign != "" && secondDigital != "") {
+        setFinish(true);
+      }
+      if (sign != "" && secondDigital == "") {
+        setSign(name);
+      }
+    }
   }
+  //вывод на дисплей
+  if (firstDigital == "" && secondDigital == "") {
+    setDisplay("0");
+  }
+  if (secondDigital == "") {
+    if (firstDigital == "") {
+      setDisplay("0");
+    } else {
+      setDisplay(firstDigital);
+    }
+  } else {
+    setDisplay(secondDigital);
+  }
+
+  console.log(
+    `firstDigital: ${firstDigital} sign: ${sign}, secondDigital: ${secondDigital} setFinish: ${finish}`
+  );
+  //анимация
+  // setIsClicked(true);
+  // setTimeout(() => {
+  //   setIsClicked(false);
+  // }, 100);
   // console.log(color.className);
   return (
     <div
